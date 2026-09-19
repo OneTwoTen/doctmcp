4316c10c8b93c5f3b84046e5ea795978132682bf
OneTwoTen <68427059+OneTwoTen@users.noreply.github.com>
2026-09-19 17:08:09 +0700

    style(server): format in-memory contract harness

--- apps/server/src/persistent-repository-contract.test.ts
+++ apps/server/src/persistent-repository-contract.test.ts
@@ -1,15 +1,16 @@
-import {
-  InMemoryDeviceCredentialRepository,
-} from "./device-credential";
+import { InMemoryDeviceCredentialRepository } from "./device-credential";
 import { InMemoryDeviceRepository } from "./device-repository";
 import { InMemoryPairingSessionRepository } from "./pairing";
 import { describePersistentRepositoryContract } from "./persistent-repository-contract-suite";
 
-describePersistentRepositoryContract("InMemory", ({ now, generateDeviceId }) => {
-  const devices = new InMemoryDeviceRepository({ now, generateDeviceId });
-  return {
-    devices,
-    credentials: new InMemoryDeviceCredentialRepository(),
-    pairings: new InMemoryPairingSessionRepository(devices, { now }),
-  };
-});
+describePersistentRepositoryContract(
+  "InMemory",
+  ({ now, generateDeviceId }) => {
+    const devices = new InMemoryDeviceRepository({ now, generateDeviceId });
+    return {
+      devices,
+      credentials: new InMemoryDeviceCredentialRepository(),
+      pairings: new InMemoryPairingSessionRepository(devices, { now }),
+    };
+  },
+);
